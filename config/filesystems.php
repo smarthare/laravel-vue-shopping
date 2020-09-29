241124fbc24cf7646d0b8dd495c2cf16ca819197
smauditoria <svob9263@gmail.com>
2020-09-29 23:08:50 +0200

    auth scaffolding done

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,12 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('layouts'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('layouts/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
@@ -79,7 +79,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('layouts/public'),
     ],
 
 ];
